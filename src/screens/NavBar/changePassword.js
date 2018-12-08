ba4758cbe10b90e343d8fbf421f3aceeb7718d61
Prajith Sai <Prajith@Prajiths-MacBook-Air.local>
2018-12-08 18:44:05 +0530

    Added change password functionality

--- src/screens/NavBar/changePassword.js
+++ src/screens/NavBar/changePassword.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-// import { LargeModal } from '../../components/Modals';
-import { Button } from 'react-bootstrap';
+import { LargeModal } from '../../components/Modals';
 
-import ReactBootstrapModal from '../../components/Modals/ReactBootstrapModal';
 import { FieldGroup } from '../../components/Form';
+import { updatePassword } from '../../actions/LoginAction';
 
 const initialForm = {
   currentPass: '',
@@ -15,86 +14,117 @@ const initialForm = {
 class ChangePassword extends Component {
   state = {
     form: initialForm,
-    errors: initialForm
+    errors: initialForm,
+    showFeedback: false,
+    feedback: ''
+  };
+
+  onSubmit = () => {
+    const { form } = this.state;
+    const errors = { ...this.state.errors };
+    let isValid = true;
+    if (form.currentPass === '') {
+      errors.currentPass = 'Current Password cannot be empty';
+      isValid = false;
+    }
+    if (form.newPass1 === '') {
+      errors.newPass1 = 'New Password cannot be empty';
+      isValid = false;
+    }
+    if (form.newPass2 === '') {
+      errors.newPass2 = 'Confirm New Password cannot be empty';
+      isValid = false;
+    }
+    if (form.newPass1 !== form.newPass2) {
+      errors.newPass2 = "New Password and Confirm New Password didn't match";
+      isValid = false;
+    }
+    if (isValid) {
+      this.props
+        .dispatch(
+          updatePassword(this.props.email, form.currentPass, form.newPass1)
+        )
+        .then(result => {
+          if (result.error !== undefined && !result.error) {
+            this.resetForm();
+            this.props.onHide();
+            this.props.showFeedback();
+          }
+        });
+    }
+    this.setState({ errors });
   };
 
   onChangeText = name => ({ target: { value } }) => {
     const form = { ...this.state.form };
     const errors = { ...this.state.errors };
     form[name] = value;
-    this.validateInput(name, value).then(newErrors =>
-      this.setState({ errors: newErrors })
-    );
+    errors[name] = '';
     this.setState({ form, errors });
   };
-  validateInput = (name, value) =>
-    new Promise(resolve => {
-      const errors = { ...this.state.errors };
-      if (value === '') {
-        // errors[name] = `${startCase(name)} cannot be empty!`;
-      } else if (value !== '') {
-        const splCharsAllowed = ['line1', 'line2', 'line3', 'orgPan'];
-        // eslint-disable-next-line
-        const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-        if (!splCharsAllowed.includes(name) && format.test(value)) {
-          errors[name] = 'Special characters are not allowed!';
-        } else if (name === 'pincode' && value.length !== 6) {
-          errors[name] = 'Pincode must be 6 digits long!';
-        } else if (name === 'orgPan' && value.length !== 10) {
-          errors[name] = 'PAN must be 10 digits long!';
-        } else {
-          errors[name] = '';
-        }
-      }
-      resolve(errors);
-    });
+
+  onHide = () => {
+    this.resetForm();
+    this.props.onHide();
+  };
+
+  resetForm = () => this.setState({ form: initialForm, errors: initialForm });
 
   render() {
     const { form, errors } = this.state;
     return (
-      <ReactBootstrapModal
-        show={this.props.show}
-        title={'Change Password'}
-        resetButton={<Button onClick={this.resetForm}>Reset</Button>}
-        cancelText={'Cancel'}
-        submitText={'Submit'}
-        onHide={this.props.onHide}
-      >
-        <form>
-          <FieldGroup
-            id="currentPass"
-            type="password"
-            label="Current Password"
-            placeholder="Enter Current Password"
-            onChange={this.onChangeText('currentPass')}
-            value={form.currentPass}
-            validationState={errors.currentPass !== '' ? 'error' : null}
-            help={errors.currentPass !== '' ? errors.currentPass : null}
-          />
-          <FieldGroup
-            id="newPass1"
-            type="password"
-            label="New Password"
-            placeholder="Enter New Password"
-            onChange={this.onChangeText('newPass1')}
-            value={form.newPass1}
-            validationState={errors.newPass1 !== '' ? 'error' : null}
-            help={errors.newPass1 !== '' ? errors.newPass1 : null}
-          />
-          <FieldGroup
-            id="newPass2"
-            type="password"
-            label="Confirm New Password"
-            placeholder="Confirm New Password"
-            onChange={this.onChangeText('newPass2')}
-            value={form.newPass2}
-            validationState={errors.newPass2 !== '' ? 'error' : null}
-            help={errors.newPass2 !== '' ? errors.newPass2 : null}
-          />
-        </form>
-      </ReactBootstrapModal>
+      <Fragment>
+        <LargeModal
+          show={this.props.show}
+          header="Change Password"
+          resetText="Reset"
+          closeText="Cancel"
+          saveText="Submit"
+          onHide={this.onHide}
+          onSave={this.onSubmit}
+          onReset={this.resetForm}
+          style={{ width: '450px', margin: '0 auto' }}
+        >
+          <form>
+            <FieldGroup
+              id="currentPass"
+              type="password"
+              label="Current Password"
+              placeholder="Enter Current Password"
+              onChange={this.onChangeText('currentPass')}
+              value={form.currentPass}
+              validationState={errors.currentPass !== '' ? 'error' : null}
+              help={errors.currentPass !== '' ? errors.currentPass : null}
+            />
+            <FieldGroup
+              id="newPass1"
+              type="password"
+              label="New Password"
+              placeholder="Enter New Password"
+              onChange={this.onChangeText('newPass1')}
+              value={form.newPass1}
+              validationState={errors.newPass1 !== '' ? 'error' : null}
+              help={errors.newPass1 !== '' ? errors.newPass1 : null}
+            />
+            <FieldGroup
+              id="newPass2"
+              type="password"
+              label="Confirm New Password"
+              placeholder="Confirm New Password"
+              onChange={this.onChangeText('newPass2')}
+              value={form.newPass2}
+              validationState={errors.newPass2 !== '' ? 'error' : null}
+              help={errors.newPass2 !== '' ? errors.newPass2 : null}
+            />
+          </form>
+        </LargeModal>
+      </Fragment>
     );
   }
 }
 
-export default connect()(ChangePassword);
+const mapStateToProps = state => ({
+  email: state.login.loggedInUser.email
+});
+
+export default connect(mapStateToProps)(ChangePassword);
